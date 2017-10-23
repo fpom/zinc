4fc41191bca9f6fd410776cbfe8d4e19c8fba53a
Franck Pommereau <franck.pommereau@gmail.com>
2017-10-23 16:34:39 +0200

    fixed tuples for untyped places

--- snakes/arcs.py
+++ snakes/arcs.py
@@ -256,10 +256,9 @@ class Tuple (InputArc, OutputArc) :
         node = ctx.ForeachToken(ctx.marking, place.name, pvar, **more)
         nest.append(node)
         nest = node.body
-        if place.type :
-            nest.append(ctx.IfTuple(record(name=place.name, type=placetype),
-                                    pvar, **more))
-            nest = nest[0].body
+        nest.append(ctx.IfTuple(record(name=place.name, type=placetype),
+                                pvar, **more))
+        nest = nest[0].body
         for path, (label, type) in self._walk(placetype) :
             if isinstance(label, (Value, Expression)) or label.source in ctx.bound :
                 var = ctx.declare.new(type)
